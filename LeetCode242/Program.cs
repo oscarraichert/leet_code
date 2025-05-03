5e013f62bccedac001335e01d109bb8002d7b936
Oscar Raichert <raichertoscar@gmail.com>
2025-05-02 22:07:03 -0300

    leet code 242

---
+++ LeetCode242/Program.cs
@@ -0,0 +1,32 @@
+﻿bool IsAnagram(string s, string t)
+{
+    if (s.Length != t.Length) return false;
+    
+    var dict1 = MakeStringDict(s);
+
+    var dict2 = MakeStringDict(t);
+
+    foreach (var entry in dict1)
+    {
+        if (dict2.ContainsKey(entry.Key) && dict2[entry.Key] == entry.Value)
+        {
+            continue;
+        }
+        else return false;
+    }
+    
+    return true;
+}
+
+Dictionary<char, int> MakeStringDict(string s1)
+{
+    return s1.Aggregate(new Dictionary<char, int>(), (x, y) =>
+    {
+        if (!x.TryAdd(y, 1))
+        {
+            x[y] += 1;
+        }
+
+        return x;
+    });
+}
