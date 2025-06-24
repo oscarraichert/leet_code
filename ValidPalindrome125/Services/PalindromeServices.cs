08cb021d8d70e1526d8f756ca8ef76cb932b4132
Oscar Raichert <raichertoscar@gmail.com>
2025-06-24 00:57:53 -0300

    :sparkles: valid palindrome

---
+++ ValidPalindrome125/Services/PalindromeServices.cs
@@ -0,0 +1,35 @@
+﻿namespace Services;
+
+public class PalindromeServices
+{
+    // "A man, a plan, a canal: Panama"
+    public static bool ValidPalindrome(string s)
+    {
+        var chars = new List<char>();
+
+        for (int i = 0; i < s.Length; i++)
+        {
+            var current = s[i];
+
+            if ((current >= 'a' && current <= 'z') || (current >= '0' && current <= '9'))
+            {
+                chars.Add(current);
+            }
+
+            else if (current >= 'A' && current <= 'Z')
+            {
+                chars.Add((char)(current + 32));
+            }
+        }
+
+        for (int i = 0, j = chars.Count - 1; i < chars.Count / 2; i++, j--)
+        {
+            if (chars[i] != chars[j])
+            {
+                return false;
+            }
+        }  
+
+        return true;
+    }
+}
---
+++ ValidPalindrome125/Tests/ValidPalindromeTests.cs
@@ -0,0 +1,13 @@
+﻿using Services;
+
+namespace Tests;
+
+public class ValidPalindromeTests
+{
+    [Theory]
+    [InlineData("A man, a plan, a canal: Panama")]
+    public void ShouldBeTrue(string s)
+    {
+        Assert.True(PalindromeServices.ValidPalindrome(s));
+    }
+}
