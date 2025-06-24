f432bddb6783df82def8e79131f84d7dbf59e8ab
Oscar Raichert <raichertoscar@gmail.com>
2025-06-23 23:42:01 -0300

    :sparkles: leet code 344 reverse string

---
+++ ReverseString/Services/ReverseStringService.cs
@@ -0,0 +1,24 @@
+﻿namespace Services;
+
+public class ReverseStringService
+{
+    public static char[] ReverseString(char[] s)
+    {
+        for (int i = 0; i < s.Length; i++)
+        {
+            var j = s.Length - i - 1;
+
+            if (i >= j)
+            {
+                break;
+            }
+
+            var currentTail = s[j];
+
+            s[j] = s[i];
+            s[i] = currentTail;
+        }
+
+        return s;
+    }
+}
---
+++ ReverseString/Tests/ReverseStringTests.cs
@@ -0,0 +1,14 @@
+﻿using Services;
+
+namespace Tests;
+
+public class ReverseStringTests
+{
+    [Theory]
+    [InlineData(new char[] {'h', 'e', 'l', 'l', 'o'}, new char[] {'o', 'l', 'l', 'e', 'h'})]
+    [InlineData(new char[] {'H', 'a', 'n', 'n', 'a', 'h'}, new char[] {'h', 'a', 'n', 'n', 'a', 'H'})]
+    public void ShouldReverseString(char[] s, char[] expected)
+    {
+        Assert.Equal(expected, ReverseStringService.ReverseString(s));
+    }
+}
